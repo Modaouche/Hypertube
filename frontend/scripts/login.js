de6e1f926102c4fb2b5d32479cb89b50b618c6c5
Ludovic Cravoisier <scrapingrat@protonmail.com>
2020-11-13 17:59:30 +0100

    scroll

--- frontend/scripts/login.js
+++ frontend/scripts/login.js
@@ -1,7 +1,22 @@
 function openForm() {
-  document.getElementById("myForm").style.display = "block";
+    document.getElementById("myForm").style.display = "block";
 }
 
 function closeForm() {
-  document.getElementById("myForm").style.display = "none";
+    document.getElementById("myForm").style.display = "none";
+    document.getElementById("login").style.display = "block";
+    document.getElementById("h1-login").innerHTML = "Login";
+    document.getElementsByClassName("pass")[0].style.display = "block";
+    document.getElementsByClassName("pass")[1].style.display = "block";
+    document.getElementsByClassName("lost")[0].style.display = "none";
+    document.getElementById("reset").style.display = "block";
+}
+
+function lostForm() {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("h1-login").innerHTML = "Lost Password";
+    document.getElementsByClassName("pass")[0].style.display = "none";
+    document.getElementsByClassName("pass")[1].style.display = "none";
+    document.getElementsByClassName("lost")[0].style.display = "none";
+    document.getElementById("reset").style.display = "block";
 }
---
+++ frontend/scripts/signup.js
@@ -0,0 +1,7 @@
+function openSignupForm() {
+    document.getElementById("signupForm").style.display = "block";
+}
+
+function closeSignupForm() {
+    document.getElementById("signupForm").style.display = "none";
+}
