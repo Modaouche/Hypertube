79c95fbfa955f0eac23e50543da5892803de09e9
Ludovic Cravoisier <scrapingrat@protonmail.com>
2020-11-13 15:45:18 +0100

    sign in form

--- frontend/scripts/login.js
+++ frontend/scripts/login.js
@@ -1,6 +1,7 @@
-document.getElementById("popup").onclick = function() {myFunction()};
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
 
-function myFunction() {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
 }
